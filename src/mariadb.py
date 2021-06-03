7923f866d87a60c81b96f2155d6e3e034ffc7c7d
Seokyong Shin <kistssy@gmail.com>
2021-06-03 17:32:05 +0900

    Update mariadb.py

--- src/mariadb.py
+++ src/mariadb.py
@@ -1,8 +1,8 @@
 import pymysql
 
-sql_state = 'INSERT INTO test VALUE (245, "test connection")'
+sql_state = 'INSERT INTO test VALUE (673, "test connection 한글")'
 
-connector = pymysql.connect(user='root', password='1234', host='127.0.0.1', database='holo', charset='utf8')
+connector = pymysql.connect(user='root', password='1234', host='127.0.0.1', database='holo')
 cursor = connector.cursor()
 cursor.execute(sql_state)
 connector.commit()
