3066b35e7d447ec33cf3aa4cbeb4dba7273b0aee
Seokyong Shin <kistssy@gmail.com>
2021-06-03 20:34:42 +0900

    src: Change sql query

--- src/mariadb.py
+++ src/mariadb.py
@@ -1,6 +1,13 @@
 import pymysql
 
-sql_query = 'INSERT INTO test VALUE (673, "test connection 한글")'
+sql_query = """
+CREATE TABLE test (
+    id INT(11) NULL DEFAULT NULL,
+    name VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+    shape VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+    image LONGBLOB NULL DEFAULT NULL
+)
+"""
 
 connector = pymysql.connect(user='root', password='1234', host='127.0.0.1', database='holo')
 cursor = connector.cursor()
