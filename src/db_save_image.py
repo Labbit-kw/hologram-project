a2c47a829449f3f79292dc4d2d27852a896c8311
Seokyong Shin <kistssy@gmail.com>
2021-06-03 19:29:34 +0900

    src: Use bytes

--- src/db_save_image.py
+++ src/db_save_image.py
@@ -2,11 +2,12 @@ import cv2
 import pymysql
 
 image = cv2.imread("../res/1.jpg", cv2.IMREAD_UNCHANGED)
+image_bytes = image.tobytes()
 
 sql_state = 'INSERT INTO test VALUE (%s, %s, %s, %s)'
 
 connector = pymysql.connect(user='root', password='1234', host='127.0.0.1', database='holo')
 cursor = connector.cursor()
-cursor.execute(sql_state, (1, 'np.ndarray', 'cv_image', image))
+cursor.execute(sql_state, (1, 'np.ndarray', 'cv_image', image_bytes))
 connector.commit()
 connector.close()
