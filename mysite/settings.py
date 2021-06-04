16f57c50bae3f58e6e4a09808e3ee7c048064959
Seokyong Shin <kistssy@gmail.com>
2021-06-04 20:52:40 +0900

    Remove apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,10 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'django.contrib.sites',
-    'django_extensions',
-    'sslserver'
-
+    'django.contrib.sites'
 ]
 
 MIDDLEWARE = [
