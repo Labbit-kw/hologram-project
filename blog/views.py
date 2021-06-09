292b8685591fc83d184118c9d2296195e8ebd6b4
Seokyong Shin <kistssy@gmail.com>
2021-06-09 20:34:11 +0900

    Refactor views.py and base.html

--- blog/views.py
+++ blog/views.py
@@ -3,17 +3,14 @@ from django.shortcuts import render
 import base64
 import os
 import pymysql
-
 import requests
 import re
 import cv2
 
+
 def saveDB(request):
     form = request.POST
-
-
     data = form['result_data'].split(',')
-
 
     for i in range(1, len(data), 2):
         data[i] = base64.b64decode(data[i])
@@ -21,7 +18,7 @@ def saveDB(request):
         with open(os.path.join('result', f'{str(i)}_img.png'), "wb") as fh:
             fh.write(data[i])
 
-    #sql_query = 'INSERT INTO image (name, image) VALUE (%s, %s)'
+    # sql_query = 'INSERT INTO image (name, image) VALUE (%s, %s)'
 
     # connector = pymysql.connect(user='root', password='1234', host='127.0.0.1', database='test')
     # cursor = connector.cursor()
@@ -29,11 +26,8 @@ def saveDB(request):
     # for j in range(1, len(data), 2):
     #     index = j//2
     #     cursor.execute(sql_query, (str(index)+'_image', data[j]))
-
-
-
+    #
     # connector.commit()
     # connector.close()
 
     return render(request, 'blog/camera_ok.html')
-
