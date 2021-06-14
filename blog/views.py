877e16d6a23ecd973da8026bf7369cb86be2d806
Rin98 <60956651+chansoopark98@users.noreply.github.com>
2021-06-14 18:02:32 +0900

    얼굴 검출

--- blog/views.py
+++ blog/views.py
@@ -6,18 +6,52 @@ import pymysql
 import requests
 import re
 import cv2
-
+import numpy
 
 def saveDB(request):
     form = request.POST
     data = form['result_data'].split(',')
 
     os.makedirs('result', exist_ok=True)
+
+    img_list = []
+
+
     for i, img in enumerate(data[1::2], start=1):
         img = base64.b64decode(img)
+        img_list.append(img)
 
         with open(os.path.join('result', f'{str(i).zfill(3)}.jpg'), "wb") as f:
             f.write(img)
+
+
+    if len(img_list) % 2 :
+        img_list.pop()
+        print("img index 홀수")
+
+    center_index = int(len(img_list) / 2)
+    print(center_index)
+
+
+
+    file_bytes = numpy.asarray(bytearray(img_list[center_index]), dtype=numpy.uint8)
+
+    cemter_image = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
+
+    module_dir = os.path.dirname(__file__)   #get current directory
+    file_path = os.path.join(module_dir, 'haarcascade_frontalface_alt.xml')
+
+    cascade = cv2.CascadeClassifier(file_path)
+    center_detected = cascade.detectMultiScale(cemter_image, scaleFactor=1.1, minNeighbors=1, minSize=(30, 30))
+
+    if len(center_detected) > 0 :
+        print("얼굴임")
+    else:
+        print("얼굴감지실패")
+
+
+
+
 
     # sql_query = 'INSERT INTO image (name, image) VALUE (%s, %s)'
 
