92200b430a838ed2c17c4437ef1cc68d48df936e
Rin98 <60956651+chansoopark98@users.noreply.github.com>
2021-06-15 17:53:42 +0900

    views.py 128 images

--- blog/views.py
+++ blog/views.py
@@ -8,46 +8,88 @@ import re
 import cv2
 import numpy
 
+def remove_img(img_list, decimal, reduce_index):
+    upper = 0
+
+    for i in range(0, reduce_index):
+        upper += decimal
+        if upper >= 1:
+            upper = round(upper % 1, 2)
+            del img_list[i * 2 + 1]
+        else:
+            del img_list[i * 2]
+
+    return img_list
+
+
 def saveDB(request):
     form = request.POST
     data = form['result_data'].split(',')
 
     os.makedirs('result', exist_ok=True)
 
-    img_list = []
+    img_list = [] # 가공 전 이미지 리스트
 
+    os.makedirs('./result/original', exist_ok=True)
+    os.makedirs('./result/output', exist_ok=True)
 
     for i, img in enumerate(data[1::2], start=1):
         img = base64.b64decode(img)
         img_list.append(img)
 
-        with open(os.path.join('result', f'{str(i).zfill(3)}.jpg'), "wb") as f:
+        with open(os.path.join('./result/original', f'{str(i).zfill(3)}.jpg'), "wb") as f:
             f.write(img)
-
 
     if len(img_list) % 2 :
         img_list.pop()
-        print("img index 홀수")
 
     center_index = int(len(img_list) / 2)
-    print(center_index)
 
-
-
-    file_bytes = numpy.asarray(bytearray(img_list[center_index]), dtype=numpy.uint8)
-
-    cemter_image = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
-
-    module_dir = os.path.dirname(__file__)   #get current directory
+    module_dir = os.path.dirname(__file__)
     file_path = os.path.join(module_dir, 'haarcascade_frontalface_alt.xml')
-
     cascade = cv2.CascadeClassifier(file_path)
-    center_detected = cascade.detectMultiScale(cemter_image, scaleFactor=1.1, minNeighbors=1, minSize=(30, 30))
 
-    if len(center_detected) > 0 :
-        print("얼굴임")
-    else:
-        print("얼굴감지실패")
+    while True:
+        file_bytes = numpy.asarray(bytearray(img_list[center_index]), dtype=numpy.uint8)
+        cemter_image = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
+
+        center_detected = cascade.detectMultiScale(cemter_image, scaleFactor=1.1, minNeighbors=1, minSize=(30, 30))
+
+        if len(center_detected) > 0:
+            first_img = img_list[0]
+            last_img = img_list[-1]
+            center_1_img = img_list[center_index]
+            center_2_img = img_list[center_index+1]
+
+            left_img = img_list[1:center_index-1]
+            right_img = img_list[center_index+2:-2]
+
+            left_img_len = len(left_img)
+            right_img_len = len(right_img)
+
+            l_reduce_index = left_img_len - 62
+            r_reduce_index = right_img_len - 62
+
+            l_reduce_per = 62 / l_reduce_index
+            r_reduce_per = 62 / r_reduce_index
+
+            l_decimal = round(l_reduce_per % 1, 2)
+            r_decimal = round(r_reduce_per % 1, 2)
+
+            left_img = remove_img(left_img, l_decimal, l_reduce_index)
+            right_img = remove_img(right_img, r_decimal, r_reduce_index)
+
+            output_img = [first_img] + left_img + [center_1_img] + [center_2_img] + right_img + [last_img]
+
+            for k in range(len(output_img)):
+                with open(os.path.join('./result/output', f'{str(k).zfill(3)}.jpg'), "wb") as f:
+                    f.write(output_img[k])
+
+            break
+        else:
+            center_index += 1
+
+
 
 
 
